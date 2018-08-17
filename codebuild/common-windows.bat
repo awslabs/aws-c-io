a9e4efe5d5328dfab23caa3bb856798abf06e0d4
graebm <graebm@amazon.com>
2018-08-17 09:26:05 -0700

    fix .bat script

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,15 +1,3 @@
-:install_library
-git clone https://github.com/awslabs/%~1.git
-cd %~1
-mkdir build
-cd build
-cmake %* -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
-msbuild.exe %~1.vcxproj /p:Configuration=Release || goto error
-msbuild.exe INSTALL.vcxproj /p:Configuration=Release || goto error
-ctest -V || goto error
-cd ../..
-EXIT /B %ERRORLEVEL%
-
 cd ../
 mkdir install
 
@@ -24,6 +12,18 @@ msbuild.exe tests/aws-c-io-tests.vcxproj /p:Configuration=Release || goto error
 ctest -V
 
 goto :EOF
+
+:install_library
+git clone https://github.com/awslabs/%~1.git
+cd %~1
+mkdir build
+cd build
+cmake %* -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
+msbuild.exe %~1.vcxproj /p:Configuration=Release || goto error
+msbuild.exe INSTALL.vcxproj /p:Configuration=Release || goto error
+ctest -V || goto error
+cd ../..
+exit /b %errorlevel%
 
 :error
 echo Failed with error #%errorlevel%.
