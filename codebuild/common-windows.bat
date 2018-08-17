6d00ff95e2a77a7f56291e053c5c5a05716657e1
graebm <graebm@amazon.com>
2018-08-17 10:38:27 -0700

    more .bat file fixes

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,4 +1,6 @@
 cd ../
+set CMAKE_ARGS=%*
+
 mkdir install
 
 CALL :install_library aws-c-common
@@ -6,9 +8,8 @@ CALL :install_library aws-c-common
 cd aws-c-io
 mkdir build
 cd build
-cmake %* -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
-msbuild.exe aws-c-io.vcxproj /p:Configuration=Release || goto error
-msbuild.exe tests/aws-c-io-tests.vcxproj /p:Configuration=Release || goto error
+cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
+cmake --build . --config Release || goto error
 ctest -V
 
 goto :EOF
@@ -18,10 +19,8 @@ git clone https://github.com/awslabs/%~1.git
 cd %~1
 mkdir build
 cd build
-cmake %* -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
-msbuild.exe %~1.vcxproj /p:Configuration=Release || goto error
-msbuild.exe INSTALL.vcxproj /p:Configuration=Release || goto error
-ctest -V || goto error
+cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
+cmake --build . --target install --config Release || goto error
 cd ../..
 exit /b %errorlevel%
 
