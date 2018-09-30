3d5e9a5b39d57c22ff10ce091dceeae49e4e158d
Jonathan M. Henson <henso@amazon.com>
2018-09-30 13:19:07 -0700

    Updated comment grammar, largely to trigger another github build hook.

--- include/aws/io/socket.h
+++ include/aws/io/socket.h
@@ -147,7 +147,7 @@ AWS_IO_API int aws_socket_init(
  * socket, all of the rules for aws_socket_close() apply here. In this case it will not fail if you use the function
  * improperly, but on some platforms you will certainly leak memory.
  *
- * If the socket has already been closed, you can safely, call this from any thread.
+ * If the socket has already been closed, you can safely call this from any thread.
  */
 AWS_IO_API void aws_socket_clean_up(struct aws_socket *socket);
 
