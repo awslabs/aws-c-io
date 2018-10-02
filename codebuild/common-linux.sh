0e88c54fa57114c8780965406ee938032420b5f8
graebm <graebm@amazon.com>
2018-10-02 13:16:33 -0700

    Depend on specific version of s2n (#36)
    * Ability to pull specific branch/changeset of a dependency
    
    * pin version for s2n dependency

--- codebuild/common-linux.sh
+++ codebuild/common-linux.sh
@@ -4,15 +4,21 @@ set -e
 
 CMAKE_ARGS="$@"
 
+# install_library <git_repo> [<commit>]
 function install_library {
     git clone https://github.com/awslabs/$1.git
     cd $1
+
+    if [ -n "$2" ]; then
+        git checkout $2
+    fi
+
     mkdir build
     cd build
 
     cmake -DCMAKE_INSTALL_PREFIX=../../install -DENABLE_SANITIZERS=ON $CMAKE_ARGS ../
     make install
-    
+
     cd ../..
 }
 
@@ -21,7 +27,7 @@ cd ../
 
 mkdir install
 
-install_library s2n
+install_library s2n 98225d7
 install_library aws-c-common
 
 cd aws-c-io
