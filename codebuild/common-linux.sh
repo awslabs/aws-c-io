2fdbbd6acb3572033da17db893d4c53ce361a8f0
Jonathan M. Henson <henso@amazon.com>
2018-11-21 10:41:26 -0800

    32 bit build fixes.

--- codebuild/common-linux.sh
+++ codebuild/common-linux.sh
@@ -29,7 +29,7 @@ cd ../
 
 mkdir install
 
-install_library s2n 55699d9ce02285b5ad1674fc08929452f994e20e
+install_library s2n 7c9069618e68214802ac7fbf45705d5f8b53135f
 install_library aws-c-common
 
 cd aws-c-io
