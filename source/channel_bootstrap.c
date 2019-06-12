f70de9ed2c0f31b314926d29d5755714261b2cbd
Justin Boswell <boswej@amazon.com>
2019-06-12 10:28:19 -0700

    clang-format

--- source/channel_bootstrap.c
+++ source/channel_bootstrap.c
@@ -524,8 +524,7 @@ static void s_on_client_connection_established(struct aws_socket *socket, int er
                 error_code);
             /* connection_args will be released after setup_callback */
             s_connection_args_setup_callback(connection_args, error_code, NULL);
-        } 
-        else {
+        } else {
             /* this socket will die silently, free up the ref to the connection_args */
             s_connection_args_release(connection_args);
         }
