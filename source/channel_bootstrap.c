8ba821e7db3c0b5295a53a18b4b24e6e9fb52e30
Bret Ambrose <bambrose@amazon.com>
2019-07-09 10:32:50 -0700

    Args were leaking on connection failures

--- source/channel_bootstrap.c
+++ source/channel_bootstrap.c
@@ -525,10 +525,10 @@ static void s_on_client_connection_established(struct aws_socket *socket, int er
                 error_code);
             /* connection_args will be released after setup_callback */
             s_connection_args_setup_callback(connection_args, error_code, NULL);
-        } else {
-            /* this socket will die silently, free up the ref to the connection_args */
-            s_connection_args_release(connection_args);
         }
+
+        /* every connection task adds a ref, so every failure or cancel needs to dec one */
+        s_connection_args_release(connection_args);
         return;
     }
 
