84ec37cc295a71cf389caeddca76875e3c227367
Justin Boswell <boswej@amazon.com>
2020-11-19 21:53:28 +0000

    Fixed cal dep in cmake

--- cmake/aws-c-io-config.cmake
+++ cmake/aws-c-io-config.cmake
@@ -4,10 +4,10 @@ if (UNIX AND NOT APPLE)
     list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
     find_dependency(s2n)
     find_dependency(LibCrypto)
-    find_dependency(aws-c-cal)
 endif()
 
 find_dependency(aws-c-common)
+find_dependency(aws-c-cal)
 
 if (BUILD_SHARED_LIBS)
     include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
