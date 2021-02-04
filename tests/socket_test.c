43b149c7378df3cf2191d47a55bee1726abd995f
Michael Graeb <graebm@amazon.com>
2021-02-04 15:52:29 -0800

    windows socket might be gone when its completion callbacks fire. ok.

--- tests/socket_test.c
+++ tests/socket_test.c
@@ -1336,6 +1336,7 @@ enum async_role {
 };
 
 static struct {
+    struct aws_allocator *allocator;
     struct aws_event_loop *event_loop;
     struct aws_socket *write_socket;
     struct aws_socket *read_socket;
@@ -1370,9 +1371,8 @@ static void s_async_read_task(struct aws_task *task, void *args, enum aws_task_s
             }
 
             /* other end must have hung up. clean up and signal completion */
-            struct aws_allocator *allocator = g_async_tester.read_socket->allocator;
             aws_socket_clean_up(g_async_tester.read_socket);
-            aws_mem_release(allocator, g_async_tester.read_socket);
+            aws_mem_release(g_async_tester.allocator, g_async_tester.read_socket);
 
             aws_mutex_lock(g_async_tester.mutex);
             g_async_tester.read_tasks_complete = true;
@@ -1385,7 +1385,7 @@ static void s_async_read_task(struct aws_task *task, void *args, enum aws_task_s
 
 static void s_async_write_completion(struct aws_socket *socket, int error_code, size_t bytes_written, void *user_data) {
     enum async_role role = *(enum async_role *)user_data;
-    aws_mem_release(socket->allocator, user_data);
+    aws_mem_release(g_async_tester.allocator, user_data);
 
     /* ensure callback is not firing synchronously from within aws_socket_write() */
     AWS_FATAL_ASSERT(!g_async_tester.currently_writing);
@@ -1400,7 +1400,7 @@ static void s_async_write_completion(struct aws_socket *socket, int error_code, 
             AWS_FATAL_ASSERT(1 == bytes_written);
             g_async_tester.currently_writing = true;
             struct aws_byte_cursor data = aws_byte_cursor_from_c_str("D");
-            enum async_role *d_role = aws_mem_acquire(socket->allocator, sizeof(enum async_role));
+            enum async_role *d_role = aws_mem_acquire(g_async_tester.allocator, sizeof(enum async_role));
             *d_role = ASYNC_ROLE_D_GOT_WRITTEN_VIA_CALLBACK;
             AWS_FATAL_ASSERT(0 == aws_socket_write(socket, &data, s_async_write_completion, d_role));
             g_async_tester.currently_writing = false;
@@ -1430,22 +1430,21 @@ static void s_async_write_task(struct aws_task *task, void *args, enum aws_task_
     (void)task;
     (void)args;
     (void)status;
-    struct aws_allocator *allocator = g_async_tester.read_socket->allocator;
 
     g_async_tester.currently_writing = true;
 
     struct aws_byte_cursor data = aws_byte_cursor_from_c_str("A");
-    enum async_role *role = aws_mem_acquire(allocator, sizeof(role));
+    enum async_role *role = aws_mem_acquire(g_async_tester.allocator, sizeof(role));
     *role = ASYNC_ROLE_A_CALLBACK_WRITES_D;
     AWS_FATAL_ASSERT(0 == aws_socket_write(g_async_tester.write_socket, &data, s_async_write_completion, role));
 
     data = aws_byte_cursor_from_c_str("B");
-    role = aws_mem_acquire(allocator, sizeof(role));
+    role = aws_mem_acquire(g_async_tester.allocator, sizeof(role));
     *role = ASYNC_ROLE_B_CALLBACK_CLEANS_UP_SOCKET;
     AWS_FATAL_ASSERT(0 == aws_socket_write(g_async_tester.write_socket, &data, s_async_write_completion, role));
 
     data = aws_byte_cursor_from_c_str("C");
-    role = aws_mem_acquire(allocator, sizeof(role));
+    role = aws_mem_acquire(g_async_tester.allocator, sizeof(role));
     *role = ASYNC_ROLE_C_IS_LAST_FROM_INITIAL_BATCH_OF_WRITES;
     AWS_FATAL_ASSERT(0 == aws_socket_write(g_async_tester.write_socket, &data, s_async_write_completion, role));
 
@@ -1525,6 +1524,7 @@ static int s_sock_write_cb_is_async(struct aws_allocator *allocator, void *ctx) 
     aws_socket_subscribe_to_readable_events(&outgoing, s_on_readable, NULL);
 
     /* set up g_async_tester */
+    g_async_tester.allocator = allocator;
     g_async_tester.event_loop = event_loop;
     g_async_tester.write_socket = &outgoing;
     g_async_tester.read_socket = server_sock;
