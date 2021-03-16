4503ce0e3f99ddc5d238301eed91f8123227312a
Dengke Tang <dengket@amazon.com>
2021-03-16 15:20:01 -0700

    update comments and pem file to check the parser

--- include/aws/io/private/pem_utils.h
+++ include/aws/io/private/pem_utils.h
@@ -10,10 +10,9 @@
 AWS_EXTERN_C_BEGIN
 
 /**
- * Cleanup Function that parses the full PEM Chain one object at a time and rewrites it following the
- * RFC formatting rules. Only well formatted pem file will be accepted.
+ * Cleanup Function that parses the full PEM Chain object once and strip the comments out for the pem parser not
+ * handling the comments.
  *
- * Performs the following operations and fixes:
  * - Garbage characters in-between PEM objects (characters before the first BEGIN or after an END and before the next
  * BEGIN) are removed
  */
