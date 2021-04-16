1bf0e2f030169ab473001db1009020fefffea1c5
Ryan Carper <rccarper@amazon.com>
2021-04-16 12:31:30 -0400

    Updating test data

--- tests/stream_test.c
+++ tests/stream_test.c
@@ -16,7 +16,7 @@ AWS_STATIC_STRING_FROM_LITERAL(s_simple_test, "SimpleTest");
 
 /* 0x1A represents the Windows end-of-file character. Having this in the test data set allows us to verify that file
  * stream reads on binary files do not terminate early on Windows.*/
-const uint8_t s_simple_binary_test[] = "abcdef\x1Ahijk";
+const uint8_t s_simple_binary_test[] = "abcdef\x1Aghijk";
 
 const char *s_test_file_name = "stream.txt";
 
