2b8907ebed26305ecad9a20092d8e4cf7caab32e
Jonathan M. Henson <henso@amazon.com>
2021-04-21 09:48:49 -0700

    Added tls 1.3 support for platforms utilizing s2n.

--- source/s2n/s2n_tls_channel_handler.c
+++ source/s2n/s2n_tls_channel_handler.c
@@ -973,13 +973,10 @@ static struct aws_tls_ctx *s_tls_ctx_new(
             s2n_config_set_cipher_preferences(s2n_ctx->s2n_config, "ELBSecurityPolicy-TLS-1-2-Ext-2018-06");
             break;
         case AWS_IO_TLSv1_3:
-            AWS_LOGF_ERROR(AWS_LS_IO_TLS, "TLS 1.3 is not supported yet.");
-            /* sorry guys, we'll add this as soon as s2n does. */
-            aws_raise_error(AWS_IO_TLS_VERSION_UNSUPPORTED);
-            goto cleanup_s2n_config;
         case AWS_IO_TLS_VER_SYS_DEFAULTS:
-        default:
-            s2n_config_set_cipher_preferences(s2n_ctx->s2n_config, "ELBSecurityPolicy-TLS-1-1-2017-01");
+            default;
+            s2n_config_set_cipher_preferences(s2n_ctx->s2n_config, "default_tls13");
+            break;
     }
 
     switch (options->cipher_pref) {
