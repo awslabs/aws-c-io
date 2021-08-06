304e2ec28b70478b953d4b16577451e218742cb1
Michael Graeb <graebm@amazon.com>
2021-08-06 10:05:38 -0700

    Add PKCS#11 headers
    Also, include them from a .c file to prove it compiles.

---
+++ include/aws/io/private/pkcs11_private.h
@@ -0,0 +1,19 @@
+#ifndef AWS_IO_PKCS11_PRIVATE_H
+#define AWS_IO_PKCS11_PRIVATE_H
+
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+#include <aws/io/pkcs11.h>
+
+/**
+ * pkcs11_private.h
+ * This file declares symbols that are private to aws-c-io but need to be
+ * accessed from multiple .c files.
+ */
+
+AWS_EXTERN_C_BEGIN
+
+AWS_EXTERN_C_END
+#endif /* AWS_IO_PKCS11_PRIVATE_H */
