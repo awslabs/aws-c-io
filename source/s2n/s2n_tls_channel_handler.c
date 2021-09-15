b2713624e6e788dca4b865fb7a81eda1dc670908
Michael Graeb <graebm@amazon.com>
2021-09-15 16:09:04 -0700

    comment

--- source/s2n/s2n_tls_channel_handler.c
+++ source/s2n/s2n_tls_channel_handler.c
@@ -948,6 +948,9 @@ static int s2n_monotonic_clock_time_nanoseconds(void *context, uint64_t *time_in
 }
 
 static int s_tls_ctx_pkcs11_setup(struct s2n_ctx *s2n_ctx, struct aws_tls_ctx_options *options) {
+    /* anything initialized in this function is cleaned up during s_s2n_ctx_destroy()
+     * so don't worry about cleaning up unless it's some tmp heap allocation */
+
     s2n_ctx->pkcs11.lib = aws_pkcs11_lib_acquire(options->pkcs11.lib);
     aws_mutex_init(&s2n_ctx->pkcs11.session_lock);
 
