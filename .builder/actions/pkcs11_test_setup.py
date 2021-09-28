1c6384e7748ad9c9d8decbe6d53bf65ccdd13331
Michael Graeb <graebm@amazon.com>
2021-09-28 21:39:53 +0000

    pkcs11_test_setup.py no longer bothers setting up tokens, since we do that from C now

--- .builder/actions/pkcs11_test_setup.py
+++ .builder/actions/pkcs11_test_setup.py
@@ -36,43 +36,12 @@ class Pkcs11TestSetup(Builder.Action):
         with open(conf_path, 'w') as conf_file:
             conf_file.write(f"directories.tokendir = {token_dir}\n")
 
-        # create a token
-        self._exec_softhsm2_util(
-            '--init-token',
-            '--free', # use any free slot
-            '--label', 'my-test-token',
-            '--pin', '0000',
-            '--so-pin', '0000')
-
-        # we need to figure out which slot the new token is in because:
-        # 1) old versions of softhsm2-util make you pass --slot <number>
-        #    (instead of accepting --token <name> like newer versions)
-        # 2) newer versions of softhsm2-util reassign new tokens to crazy
-        #    slot numbers (instead of simply using 0 like older versions)
-        slot = self._get_token_slots()[0]
-
-        # add private key to token
-        resources_dir = '../../tests/resources'
-        this_dir = os.path.dirname(__file__)
-        resources_dir = os.path.realpath(os.path.join(this_dir, resources_dir))
-        self._exec_softhsm2_util(
-            '--import', os.path.join(resources_dir, 'unittests.p8'),
-            '--slot', str(slot),
-            '--label', 'my-test-key',
-            '--id', 'BEEFCAFE', # ID is hex
-            '--pin', '0000')
-
-        # for logging's sake, print the new state of things
-        self._exec_softhsm2_util('--show-slots', '--pin', '0000')
+        # sanity check SoftHSM is working
+        self._exec_softhsm2_util('--show-slots')
 
         # set env vars for tests
         self._setenv('TEST_PKCS11_LIB', softhsm_lib)
-        self._setenv('TEST_PKCS11_TOKEN_LABEL', 'my-test-token')
-        self._setenv('TEST_PKCS11_PIN', '0000')
-        self._setenv('TEST_PKCS11_PKEY_LABEL', 'my-test-key')
-        self._setenv('TEST_PKCS11_CERT_FILE', os.path.join(resources_dir, 'unittests.crt'))
-        self._setenv('TEST_PKCS11_CA_FILE', os.path.join(resources_dir, 'unittests.crt'))
-
+        self._setenv('TEST_PKCS11_TOKEN_DIR', token_dir)
 
     def _find_softhsm_lib(self):
         """Return path to SoftHSM2 shared lib, or None if not found"""
