bb78b42a446ac5d53a0cd1e3fd69f80a12acc254
Michael Graeb <graebm@amazon.com>
2021-10-28 12:59:03 -0700

    ifdef differently

--- tests/pkcs11_test.c
+++ tests/pkcs11_test.c
@@ -550,17 +550,21 @@ static int s_test_pkcs11_session_tests(struct aws_allocator *allocator, void *ct
     /* Reset PKCS#11 tokens and load library */
     ASSERT_SUCCESS(s_pkcs11_tester_init(allocator));
 
-    /* Assert that we fail to create a session for an invalid slot.
+    /* Assert that creating a session for an invalid slot fails.
      *
      * NOTE: We omit this part of the test when AddressSanitizer is being used,
-     * because SoftHSM v2.2 triggers the sanitizer in this scenario.
-     * I've tried using a suppression file to ignore it, but the suppression isn't
+     * because SoftHSM v2.2 triggers it in this scenario. I've tried using a
+     * suppression file to ignore the issue, but the suppression isn't
      * working and I still don't understand why after 1+ hours of effort.
      * But this ifdef does the trick so that's what I'm doing. */
-
-#if !(defined(__has_feature) && __has_feature(address_sanitizer))
+#if defined(__has_feature)
+#    if __has_feature(address_sanitizer)
+#        define ADDRESS_SANITIZER_ENABLED 1
+#    endif
+#endif
+#if !ADDRESS_SANITIZER_ENABLED
     CK_SESSION_HANDLE session = CK_INVALID_HANDLE;
-    /* We havent created any slots and we are starting from a clean softhsm, so any slot value is invalid */
+    /* we haven't created any slots and we are starting from a clean softhsm, so any slot value is invalid. */
     CK_SLOT_ID slot = 1;
     ASSERT_FAILS(aws_pkcs11_lib_open_session(s_pkcs11_tester.lib, slot, &session /*out*/));
 #endif
