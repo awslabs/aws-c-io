9c7fa1228881efb9b1efa1838a8697265da52bbb
Justin Boswell <boswej@amazon.com>
2021-11-03 16:58:16 -0700

    Remove hack

--- source/posix/host_resolver.c
+++ source/posix/host_resolver.c
@@ -32,7 +32,7 @@ int aws_default_dns_resolve(
 
     /* Android would prefer NO HINTS IF YOU DON'T MIND, SIR */
 #if defined(ANDROID)
-    int err_code = getaddrinfo(hostname_cstr, "80", NULL, &result);
+    int err_code = getaddrinfo(hostname_cstr, NULL, NULL, &result);
 #else
     struct addrinfo hints;
     AWS_ZERO_STRUCT(hints);
