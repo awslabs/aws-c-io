1109279a323d6687e76a62282234f7c3bdb650be
Zhihui Xia <zhvxia@amazon.com>
2022-10-25 10:35:51 -0700

    adjust comments

--- include/aws/io/private/pki_utils.h
+++ include/aws/io/private/pki_utils.h
@@ -41,8 +41,8 @@ AWS_IO_API int aws_decode_pem_to_buffer_list(
 /**
  * Decodes a PEM file at 'filename' and adds the results to 'cert_chain_or_key' if successful.
  * Otherwise, 'cert_chain_or_key' will be empty.
- * The passed-in parameter 'cert_chain_or_key' should be intiazlied and empty array_list with
- * item type 'struct aws_byte_buf' in value.
+ * The passed-in parameter 'cert_chain_or_key' should be empty and dynamically initialized array_list
+ * with item type 'struct aws_byte_buf' in value.
  * This code is slow, and it allocates, so please try not to call this in the middle of
  * something that needs to be fast or resource sensitive.
  */
