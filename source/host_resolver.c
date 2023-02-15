48379c676b02752834147f038ba553ca27277ccf
Waqar Ahmed Khan <waahm7@gmail.com>
2023-02-15 11:58:13 -0800

    remove todo

--- source/host_resolver.c
+++ source/host_resolver.c
@@ -386,7 +386,6 @@ static void s_clear_default_resolver_entry_table(
     aws_hash_table_clear(table);
 }
 
-/* TODO: add a callback to notify when purge has completed */
 static int resolver_purge_cache(struct aws_host_resolver *resolver) {
     struct default_host_resolver *default_host_resolver = resolver->impl;
     aws_mutex_lock(&default_host_resolver->resolver_lock);
