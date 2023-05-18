a1c24c691256e29a747e2c3ac2a4111a45109f0b
Noah Beard <ncbeard@amazon.com>
2023-05-18 15:42:38 -0400

    Derp - fix the fatal asserts

--- source/posix/socket.c
+++ source/posix/socket.c
@@ -1911,11 +1911,11 @@ bool aws_socket_is_open(struct aws_socket *socket) {
 
 void aws_socket_endpoint_init_local_address_for_test(struct aws_socket_endpoint *endpoint) {
     struct aws_uuid uuid;
-    AWS_FATAL_ASSERT(aws_uuid_init(&uuid));
+    AWS_FATAL_ASSERT(aws_uuid_init(&uuid) == AWS_OP_SUCCESS);
     char uuid_str[AWS_UUID_STR_LEN] = {0};
     struct aws_byte_buf uuid_buf = aws_byte_buf_from_array(uuid_str, sizeof(uuid_str));
     uuid_buf.len = 0;
-    AWS_FATAL_ASSERT(aws_uuid_to_str(&uuid, &uuid_buf));
+    AWS_FATAL_ASSERT(aws_uuid_to_str(&uuid, &uuid_buf) == AWS_OP_SUCCESS);
     snprintf(
-        endpoint->address, sizeof(endpoint->address), "testsock" PRInSTR ".sock" PRInSTR, AWS_BYTE_BUF_PRI(uuid_buf));
+        endpoint->address, sizeof(endpoint->address), "testsock" PRInSTR ".sock", AWS_BYTE_BUF_PRI(uuid_buf));
 }
