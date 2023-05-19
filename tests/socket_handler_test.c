84d6855fc0b60c470d8adeb9870b2753ed0da077
Noah Beard <ncbeard@amazon.com>
2023-05-19 14:14:26 -0400

    Missed one - add clock.h back to another test header

--- tests/socket_handler_test.c
+++ tests/socket_handler_test.c
@@ -9,6 +9,7 @@
 #include <aws/io/statistics.h>
 
 #include <aws/common/atomics.h>
+#include <aws/common/clock.h>
 #include <aws/common/condition_variable.h>
 
 #include <aws/testing/aws_test_harness.h>
