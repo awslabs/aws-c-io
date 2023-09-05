09af9979f4e3eaea4222d9fc514728d716d0721f
Dengke Tang <dengket@amazon.com>
2023-09-05 15:31:52 -0700

    now the latest is 13 and fix the compiling warning

--- source/tracing.c
+++ source/tracing.c
@@ -11,7 +11,7 @@ __itt_string_handle *tracing_event_loop_run_tasks;
 __itt_string_handle *tracing_event_loop_event;
 __itt_string_handle *tracing_event_loop_events;
 
-void aws_io_tracing_init() {
+void aws_io_tracing_init(void) {
     io_tracing_domain = __itt_domain_create("aws.c.io");
     tracing_input_stream_read = __itt_string_handle_create("Read:InputStream");
     tracing_event_loop_run_tasks = __itt_string_handle_create("RunTasks:EventLoop");
