d4a6bd58fca2847797a6ec9c87ccbc4547249336
Dengke Tang <dengket@amazon.com>
2024-02-21 09:42:59 -0800

    make sure that part of code actually runs

--- source/darwin/darwin_pki_utils.c
+++ source/darwin/darwin_pki_utils.c
@@ -224,6 +224,7 @@ int aws_import_public_and_private_keys_to_identity(
         aws_array_list_get_at_ptr(&cert_chain_list, (void **)&root_cert_ptr, 0);
         AWS_ASSERT(root_cert_ptr);
         CFDataRef root_cert_data = CFDataCreate(cf_alloc, root_cert_ptr->data.buffer, root_cert_ptr->data.len);
+        AWS_FATAL_ASSERT(false);
         if (!root_cert_data) {
             AWS_LOGF_ERROR(AWS_LS_IO_PKI, "static: failed creating root cert data.");
             result = aws_raise_error(AWS_ERROR_SYS_CALL_FAILURE);
