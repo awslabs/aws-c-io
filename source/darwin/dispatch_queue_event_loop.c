d06d3fd26c9dc6e03ec6dc087cd0815581e3b020
Zhihui Xia <zhvxia@amazon.com>
2024-08-30 08:52:35 -0700

    add thread count into dispatch queue

--- source/darwin/dispatch_queue_event_loop.c
+++ source/darwin/dispatch_queue_event_loop.c
@@ -62,7 +62,7 @@ struct dispatch_scheduling_state {
 struct scheduled_service_entry {
     struct aws_allocator *allocator;
     uint64_t timestamp;
-    struct aws_linked_list node;
+    struct aws_linked_list_node node;
     struct aws_event_loop *loop; // might eventually need to be ref-counted for cleanup?
 };
 
@@ -126,15 +126,17 @@ static void s_finalize(void* context)
     struct aws_event_loop* event_loop = context;
     struct dispatch_loop *dispatch_loop = event_loop->impl_data;
     AWS_LOGF_INFO(AWS_LS_IO_EVENT_LOOP, "id=%p: Dispatch Queue Finalized", (void *)event_loop);
-    aws_ref_count_release(event_loop);
+    aws_ref_count_release(&dispatch_loop->ref_count);
 }
 
 
-static void* s_dispatch_event_loop_destroy(void* context){
+static void s_dispatch_event_loop_destroy(void* context){
     // release dispatch loop
     struct aws_event_loop * event_loop = context;
     struct dispatch_loop* dispatch_loop = event_loop->impl_data;
-    
+
+    aws_thread_decrement_unjoined_count();
+
     aws_mutex_clean_up(&dispatch_loop->synced_data.lock);
     aws_mem_release(dispatch_loop->allocator, dispatch_loop);
     aws_event_loop_clean_up_base(event_loop);
@@ -149,7 +151,7 @@ struct aws_event_loop *aws_event_loop_new_dispatch_queue_with_options(
     AWS_PRECONDITION(options->clock);
 
     struct aws_event_loop *loop = aws_mem_calloc(alloc, 1, sizeof(struct aws_event_loop));
-    
+
     AWS_LOGF_INFO(AWS_LS_IO_EVENT_LOOP, "id=%p: Initializing dispatch_queue event-loop", (void *)loop);
     if (aws_event_loop_init_base(loop, alloc, options->clock)) {
         goto clean_up_loop;
@@ -157,7 +159,7 @@ struct aws_event_loop *aws_event_loop_new_dispatch_queue_with_options(
 
     struct dispatch_loop *dispatch_loop = aws_mem_calloc(alloc, 1, sizeof(struct dispatch_loop));
     aws_ref_count_init(&dispatch_loop->ref_count, loop, s_dispatch_event_loop_destroy);
-    
+
 
     dispatch_loop->dispatch_queue =
         dispatch_queue_create("com.amazonaws.commonruntime.eventloop", DISPATCH_QUEUE_SERIAL);
@@ -201,6 +203,7 @@ struct aws_event_loop *aws_event_loop_new_dispatch_queue_with_options(
     dispatch_set_context(dispatch_loop->dispatch_queue, loop);
     // Definalizer will be called on dispatch queue ref drop to 0
     dispatch_set_finalizer_f(dispatch_loop->dispatch_queue, &s_finalize);
+    aws_thread_increment_unjoined_count();
 
 
     return loop;
@@ -241,7 +244,7 @@ static void s_destroy(struct aws_event_loop *event_loop) {
         }
 
         while (!aws_linked_list_empty(&dispatch_loop->local_cross_thread_tasks)) {
-            struct aws_linked_list_node *node = aws_linked_list_pop_front(&dispatch_loop->synced_data.cross_thread_tasks);
+            struct aws_linked_list_node *node = aws_linked_list_pop_front(&dispatch_loop->local_cross_thread_tasks);
             struct aws_task *task = AWS_CONTAINER_OF(node, struct aws_task, node);
             task->fn(task, task->arg, AWS_TASK_STATUS_CANCELED);
         }
@@ -357,7 +360,7 @@ void end_iteration(struct scheduled_service_entry *entry) {
 done:
     AWS_LOGF_INFO(AWS_LS_IO_EVENT_LOOP, "id=%p: End of Iteration, start to destroy service entry.", (void *)entry->loop);
     aws_mutex_unlock(&loop->synced_data.lock);
-    
+
     scheduled_service_entry_destroy(entry);
 }
 
@@ -369,12 +372,13 @@ void run_iteration(void *context) {
     struct aws_event_loop* event_loop = entry->loop;
     if(event_loop == NULL) return;
     struct dispatch_loop* dispatch_loop = event_loop->impl_data;
-    aws_event_loop_register_tick_start(event_loop);
+
 
     if (!begin_iteration(entry)) {
         return;
     }
 
+    aws_event_loop_register_tick_start(event_loop);
     // run the full iteration here: local cross-thread tasks
     AWS_LOGF_TRACE(AWS_LS_IO_EVENT_LOOP, "id=%p: processing cross-thread tasks", (void *)dispatch_loop);
 
@@ -425,19 +429,36 @@ void try_schedule_new_iteration(struct aws_event_loop *loop, uint64_t timestamp)
 static void s_schedule_task_common(struct aws_event_loop *event_loop, struct aws_task *task, uint64_t run_at_nanos) {
     struct dispatch_loop *dispatch_loop = event_loop->impl_data;
 
+
+    // if(aws_linked_list_node_is_in_list(&task->node)){
+    //     if (run_at_nanos == 0) {
+    //                 aws_task_scheduler_schedule_now(&dispatch_loop->scheduler, task);
+    //             } else {
+    //                 aws_task_scheduler_schedule_future(&dispatch_loop->scheduler, task, run_at_nanos);
+    //             }
+    //     return;
+    // }
+
     aws_mutex_lock(&dispatch_loop->synced_data.lock);
+    bool should_schedule = false;
 
     bool is_empty = aws_linked_list_empty(&dispatch_loop->synced_data.cross_thread_tasks);
+
     aws_linked_list_push_back(&dispatch_loop->synced_data.cross_thread_tasks, &task->node);
     if (is_empty) {
         if (!dispatch_loop->synced_data.scheduling_state.is_executing_iteration) {
             if (should_schedule_iteration(&dispatch_loop->synced_data.scheduling_state.scheduled_services, 0)) {
-                try_schedule_new_iteration(event_loop, 0);
+                should_schedule = true;
             }
         }
     }
 
     aws_mutex_unlock(&dispatch_loop->synced_data.lock);
+
+    if(should_schedule)
+    {
+        try_schedule_new_iteration(event_loop, 0);
+    }
 }
 
 static void s_schedule_task_now(struct aws_event_loop *event_loop, struct aws_task *task) {
@@ -452,9 +473,7 @@ static void s_cancel_task(struct aws_event_loop *event_loop, struct aws_task *ta
     AWS_LOGF_TRACE(AWS_LS_IO_EVENT_LOOP, "id=%p: cancelling task %p", (void *)event_loop, (void *)task);
     struct dispatch_loop *dispatch_loop = event_loop->impl_data;
 
-    dispatch_async(dispatch_loop->dispatch_queue, ^{
-      aws_task_scheduler_cancel_task(&dispatch_loop->scheduler, task);
-    });
+    aws_task_scheduler_cancel_task(&dispatch_loop->scheduler, task);
 }
 
 static int s_connect_to_dispatch_queue(struct aws_event_loop *event_loop, struct aws_io_handle *handle) {
