20098df1ca843aaff9f6a4165e54979e51573d54
Vera Xia <zhvxia@amazon.com>
2024-10-03 09:26:21 -0700

    setup cmake flags for macos ci

--- tests/event_loop_test.c
+++ tests/event_loop_test.c
@@ -78,7 +78,11 @@ static int s_test_event_loop_xthread_scheduled_tasks_execute(struct aws_allocato
     ASSERT_TRUE(task_args.invoked);
     aws_mutex_unlock(&task_args.mutex);
 
+// The dispatch queue will schedule tasks on thread pools, it is unpredicatable which thread we run the task on,
+// therefore we do not validate the thread id for disaptch queue.
+#ifndef AWS_USE_DISPATCH_QUEUE
     ASSERT_FALSE(aws_thread_thread_id_equal(task_args.thread_id, aws_thread_current_thread_id()));
+#endif
 
     /* Test "now" tasks */
     task_args.invoked = false;
@@ -150,7 +154,11 @@ static int s_test_event_loop_canceled_tasks_run_in_el_thread(struct aws_allocato
         &task1_args.condition_variable, &task1_args.mutex, s_task_ran_predicate, &task1_args));
     ASSERT_TRUE(task1_args.invoked);
     ASSERT_TRUE(task1_args.was_in_thread);
+// The dispatch queue will schedule tasks on thread pools, it is unpredicatable which thread we run the task on,
+// therefore we do not validate the thread id for disaptch queue.
+#ifndef AWS_USE_DISPATCH_QUEUE
     ASSERT_FALSE(aws_thread_thread_id_equal(task1_args.thread_id, aws_thread_current_thread_id()));
+#endif
     ASSERT_INT_EQUALS(AWS_TASK_STATUS_RUN_READY, task1_args.status);
     aws_mutex_unlock(&task1_args.mutex);
 
@@ -164,7 +172,11 @@ static int s_test_event_loop_canceled_tasks_run_in_el_thread(struct aws_allocato
     aws_mutex_unlock(&task2_args.mutex);
 
     ASSERT_TRUE(task2_args.was_in_thread);
+// The dispatch queue will schedule tasks on thread pools, it is unpredicatable which thread we run the task on,
+// therefore we do not validate the thread id for disaptch queue.
+#ifndef AWS_USE_DISPATCH_QUEUE
     ASSERT_TRUE(aws_thread_thread_id_equal(task2_args.thread_id, aws_thread_current_thread_id()));
+#endif
     ASSERT_INT_EQUALS(AWS_TASK_STATUS_CANCELED, task2_args.status);
 
     return AWS_OP_SUCCESS;
