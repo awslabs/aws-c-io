187b704a360d96b3f13da0f4635ba5e552db9c37
Steve Kim <sbstevek@amazon.com>
2024-10-28 09:02:31 -0700

    another missed revert

--- tests/tls_handler_test.c
+++ tests/tls_handler_test.c
@@ -1530,13 +1530,9 @@ AWS_TEST_CASE(
     tls_client_channel_negotiation_override_legacy_crypto_tls10,
     s_tls_client_channel_negotiation_override_legacy_crypto_tls10_fn)
 
-static void s_lower_tls_version_to_tls11(struct aws_tls_ctx_options *options) {
-    aws_tls_ctx_options_set_minimum_tls_version(options, AWS_IO_TLSv1_1);
-}
-
 static int s_tls_client_channel_negotiation_success_legacy_crypto_tls11_fn(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
-    return s_verify_good_host(allocator, s_legacy_crypto_tls11_host_name, 1011, &s_lower_tls_version_to_tls11);
+    return s_verify_good_host(allocator, s_legacy_crypto_tls11_host_name, 1011, NULL);
 }
 
 AWS_TEST_CASE(
