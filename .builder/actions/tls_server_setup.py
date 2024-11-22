ce8e3d29ea841190b879631725141763686990e2
Igor Abdrakhimov <igorabd@amazon.com>
2024-11-22 15:55:19 -0800

    Remove -trace

--- .builder/actions/tls_server_setup.py
+++ .builder/actions/tls_server_setup.py
@@ -38,7 +38,6 @@ class TlsServerSetup(Builder.Action):
                                "-alpn", "x-amzn-mqtt-ca",
                                "-tls1_3",  # Allow TLS 1.3 connections only
                                "-verify", "1",  # Verify client's certificate
-                               "-trace"
                                ], cwd=dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(1)
         p.poll()
