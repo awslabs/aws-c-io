7d665486b28d4eb8df4add37f906ee8392c9e7b9
Igor Abdrakhimov <igorabd@amazon.com>
2024-11-22 13:49:57 -0800

    Run local TLS server

---
+++ .builder/actions/tls_server_setup.py
@@ -0,0 +1,42 @@
+"""
+Setup local TLS server for tests
+"""
+
+import Builder
+
+import os
+import sys
+import subprocess
+import atexit
+
+
+class TlsServerSetup(Builder.Action):
+    """
+    Set up this machine for running the mock server test
+
+    This action should be run in the 'pre_build_steps' or 'build_steps' stage.
+    """
+
+    def run(self, env):
+        if not env.project.needs_tests(env):
+            print("Skipping TLS server setup because tests disabled for project")
+            return
+
+        self.env = env
+
+        base_dir = os.path.dirname(os.path.realpath(__file__))
+        dir = os.path.join(base_dir, "..", "..", "tests", "resources")
+
+        p1 = subprocess.Popen(["openssl.exe", "s_server",
+                               "-accept", "1443",
+                               "-key", "server.key",
+                               "-cert", "server.crt",
+                               "-CAfile", "server_chain.crt",
+                               "-alpn", "x-amzn-mqtt-ca",
+                               "-tls1_3",  # Allow TLS 1.3 connections only
+                               "-verify", "1"  # Verify client's certificate
+                               ], cwd=dir)
+
+        @atexit.register
+        def close_tls_server():
+            p1.terminate()
