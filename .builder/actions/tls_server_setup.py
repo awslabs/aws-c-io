6252e28a27d1f54708cd3b07ff4cc1d66b0429c3
Igor Abdrakhimov <igorabd@amazon.com>
2024-11-22 16:31:15 -0800

    debugging

--- .builder/actions/tls_server_setup.py
+++ .builder/actions/tls_server_setup.py
@@ -50,7 +50,7 @@ class TlsServerSetup(Builder.Action):
                                "-CAfile", "server_chain.crt",
                                "-alpn", "x-amzn-mqtt-ca",
                                "-tls1_3",  # Allow TLS 1.3 connections only
-                               "-verify", "1",  # Verify client's certificate
+                               # "-verify", "1",  # Verify client's certificate
                                ], cwd=dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(1)
         p.poll()
