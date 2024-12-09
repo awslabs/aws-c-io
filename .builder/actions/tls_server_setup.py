87d9a3840e15f0a02fa51dbfb073bb3bd2eca9cb
Igor Abdrakhimov <igorabd@amazon.com>
2024-12-09 11:25:22 -0800

    Clean up setting up tls server

--- .builder/actions/tls_server_setup.py
+++ .builder/actions/tls_server_setup.py
@@ -30,58 +30,9 @@ class TlsServerSetup(Builder.Action):
 
         print("Running openssl TLS server")
 
-
-        p1 = subprocess.Popen(["openssl.exe", "version", "-a",
-                              ], cwd=dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        time.sleep(1)
-        p1.poll()
-        print("Return code for p1 is {}".format(p1.returncode))
-        print("=== stdout 1:")
-        for c in iter(lambda: p1.stdout.read(1), b""):
-            sys.stdout.buffer.write(c)
-        print("=== stderr 1:")
-        for c in iter(lambda: p1.stderr.read(1), b""):
-            sys.stdout.buffer.write(c)
-
-        p1 = subprocess.Popen(["certutil", "-addstore", "Root", "tls13_root_ca.pem.crt",
-                              ], cwd=dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        time.sleep(1)
-        p1.poll()
-        print("Return code for p1 is {}".format(p1.returncode))
-        print("=== stdout 1:")
-        for c in iter(lambda: p1.stdout.read(1), b""):
-            sys.stdout.buffer.write(c)
-        print("=== stderr 1:")
-        for c in iter(lambda: p1.stderr.read(1), b""):
-            sys.stdout.buffer.write(c)
-
-        # p = subprocess.Popen(["openssl.exe", "s_server",
-        #                        "-accept", "localhost:59443",
-        #                        "-key", "tls13.key",
-        #                        "-cert", "tls13.pem.crt",
-        #                        "-chainCAfile", "tls13_root_ca.pem.crt",
-        #                        "-alpn", "x-amzn-mqtt-ca",
-        #                        "-debug", "-state",
-        #                        ], cwd=dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
         python_path = sys.executable
         p = subprocess.Popen([python_path, "main.py",
                               ], cwd=dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        time.sleep(1)
-        p.poll()
-        print("Return code is {}".format(p.returncode))
-
-        p2 = subprocess.Popen(["openssl.exe", "s_client",
-                               "-connect", "localhost:59443",
-                               "-debug", "-state",
-                               "-servername", "localhost",
-                               ], cwd=dir, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        p2.poll()
-        print("client Return code is {}".format(p2.returncode))
-
-        out2, err2 = p2.communicate(input=b'Q')
-        print("=== client stdout:\n{}".format(out2))
-        print("=== client stderr:\n{}".format(err2))
 
         @atexit.register
         def close_tls_server():
