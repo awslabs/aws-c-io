cd125b03b1064537523838ee38f85c09e6c57c09
Steve Kim <sbstevek@amazon.com>
2025-01-30 08:27:58 -0800

    revert event_loop override

--- source/event_loop.c
+++ source/event_loop.c
@@ -18,8 +18,7 @@
 #ifdef AWS_USE_APPLE_NETWORK_FRAMEWORK
 static enum aws_event_loop_type s_default_event_loop_type_override = AWS_EVENT_LOOP_DISPATCH_QUEUE;
 #else
-// DEBUG WIP CHANGE THIS BACK TO AWS_EVENT_LOOP_PLATFORM_DEFAULT
-static enum aws_event_loop_type s_default_event_loop_type_override = AWS_EVENT_LOOP_DISPATCH_QUEUE;
+static enum aws_event_loop_type s_default_event_loop_type_override = AWS_EVENT_LOOP_PLATFORM_DEFAULT;
 #endif
 
 struct aws_event_loop *aws_event_loop_new_default(struct aws_allocator *alloc, aws_io_clock_fn *clock) {
