0d3293221afc10fa936f54dc844e951e6faa5f61
Igor Abdrakhimov <igorabd@amazon.com>
2026-04-27 15:30:41 -0700

    Improve tls server

--- .builder/actions/tls_server_setup.py
+++ .builder/actions/tls_server_setup.py
@@ -1,14 +1,12 @@
 """
-Setup local TLS server for tests
+Setup local TLS servers for tests
 """
 
 import Builder
 
-import os
-import sys
+from pathlib import Path
 import subprocess
 import atexit
-import time
 
 
 class TlsServerSetup(Builder.Action):
@@ -18,6 +16,17 @@ class TlsServerSetup(Builder.Action):
     This action should be run in the 'pre_build_steps' or 'build_steps' stage.
     """
 
+    @staticmethod
+    def cleanup_tls_server(tls_server_process):
+        tls_server_process.terminate()
+        out, err = tls_server_process.communicate()
+        print("TLS server stdout:")
+        for line in out.splitlines():
+            print(f"  = {line.decode('utf-8')}")
+        print("TLS server stderr:")
+        for line in err.splitlines():
+            print(f"  = {line.decode('utf-8')}")
+
     def run(self, env):
         if not env.project.needs_tests(env):
             print("Skipping TLS server setup because tests disabled for project")
@@ -25,19 +34,29 @@ class TlsServerSetup(Builder.Action):
 
         self.env = env
 
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        dir = os.path.join(base_dir, "..", "..", "tests", "tls_server")
+        root_dir = Path(__file__).resolve().parent / '..' / '..'
+        tls_server_dir = root_dir / 'tests' / 'tls_server'
+        resource_dir = root_dir / 'tests' / 'resources'
 
-        print("Running openssl TLS server")
+        print("Running TLS servers")
 
-        python_path = sys.executable
-        p = subprocess.Popen([python_path, "tls_server.py",
-                              ], cwd=dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        python_path = env.config['variables']['python']
+
+        tls12_server_process = subprocess.Popen(
+            [python_path, tls_server_dir / 'tls_server.py', '--port', '58443', '--resource-dir', resource_dir,
+             '--min-tls', '1.2',
+             '--max-tls', '1.2'],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        tls13_server_process = subprocess.Popen(
+            [python_path, tls_server_dir / 'tls_server.py', '--port', '59443', '--resource-dir', resource_dir,
+             '--min-tls', '1.3',
+             '--max-tls', '1.3'],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         @atexit.register
-        def close_tls_server():
-            print("Terminating openssl TLS server")
-            p.terminate()
-            out, err = p.communicate()
-            print("TLS server stdout:\n{}".format(out))
-            print("TLS server stderr:\n{}".format(err))
+        def close_tls_servers():
+            print('Terminating TLS 1.2 server')
+            TlsServerSetup.cleanup_tls_server(tls12_server_process)
+            print('Terminating TLS 1.3 server')
+            TlsServerSetup.cleanup_tls_server(tls13_server_process)
