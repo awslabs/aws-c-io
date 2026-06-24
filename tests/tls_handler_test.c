378c586adfa72c96deef0b3d1215d67755f83af0
Dengke Tang <dengket@amazon.com>
2026-06-24 17:53:40 +0000

    comment tweak

--- tests/tls_handler_test.c
+++ tests/tls_handler_test.c
@@ -39,9 +39,7 @@ bool s_is_badssl_being_flaky(const struct aws_string *host_name, int error_code)
     if (strstr(aws_string_c_str(host_name), "badssl.com") != NULL) {
         if (error_code == AWS_IO_SOCKET_TIMEOUT || error_code == AWS_IO_TLS_NEGOTIATION_TIMEOUT ||
             error_code == AWS_IO_SOCKET_CLOSED) {
-            fprintf(
-                AWS_TESTING_REPORT_FD,
-                "Warning: badssl.com is timing out right now. Maybe run the test again later?\n");
+            fprintf(AWS_TESTING_REPORT_FD, "Warning: badssl.com is flaky. Allow the transiet error to happen\n");
             return true;
         }
     }
