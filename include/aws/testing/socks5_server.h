ba0e2217d57c631f2eebc223762c4600a669f663
Bret Ambrose <bretambrose@gmail.com>
2026-08-31 10:38:56 -0700

    Bad user data usage

--- include/aws/testing/socks5_server.h
+++ include/aws/testing/socks5_server.h
@@ -1283,18 +1283,6 @@ static void s_aws_socks5_server_bootstrap_on_accept_channel_setup_fn(
     aws_channel_slot_set_handler(slot, &tunnel->to_client_handler);
 }
 
-static void s_aws_socks5_server_bootstrap_on_accept_channel_shutdown_fn(
-    struct aws_server_bootstrap *bootstrap,
-    int error_code,
-    struct aws_channel *channel,
-    void *user_data) {
-    (void)bootstrap;
-    (void)channel;
-
-    struct aws_socks5_tunnel *tunnel = user_data;
-    s_aws_socks5_tunnel_update_error_code(tunnel, error_code);
-}
-
 static void s_aws_socks5_server_bootstrap_on_server_listener_destroy_fn(
     struct aws_server_bootstrap *bootstrap,
     void *user_data) {
@@ -1332,7 +1320,6 @@ static int aws_socks5_server_begin_accept(struct aws_socks5_server *server) {
         .tls_options = NULL,
         .setup_callback = s_aws_socks5_server_bootstrap_on_listener_setup_fn,
         .incoming_callback = s_aws_socks5_server_bootstrap_on_accept_channel_setup_fn,
-        .shutdown_callback = s_aws_socks5_server_bootstrap_on_accept_channel_shutdown_fn,
         .destroy_callback = s_aws_socks5_server_bootstrap_on_server_listener_destroy_fn,
         .enable_read_back_pressure = false,
         .user_data = server,
